ef8a2af1f159e12599cc94f21ddabb1706e41e0c
theduke <chris@theduke.at>
2017-10-07 11:38:39 +0200

    Add packages: libgit2-dev, cmake, libsqlite3-dev

--- bin/build.sh
+++ bin/build.sh
@@ -18,7 +18,20 @@ esac
 
 cd /root
 
-PACKAGES="curl build-essential git openssl libssl-dev pkg-config libssh2-1 libssh2-1-dev"
+PACKAGES="curl \
+build-essential \
+git \
+openssl \
+libssl-dev \
+pkg-config \
+libssh2-1 \
+libssh2-1-dev \
+cmake \
+libsqlite3-dev \
+libgit2-dev \
+"
+
+echo $PACKAGES
 
 # Instal rustup and rust.
 apt-get update &&
